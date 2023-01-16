3e8a29a1221382e3a004f663056a71476bb26e3d
Your Name01~ <you@example.com>
2023-01-16 23:30:12 +0600

    Web-WOrdker completed!

--- main.js
+++ main.js
@@ -1,68 +1,80 @@
+var statusDisplay;
 var worker;
+var searchButton;
+
+window.onload = function() {
+  statusDisplay = document.getElementById("status");
+  searchButton = document.getElementById("searchButton");
+};
 
 function doSearch() {
-  // Disable the button, so the user can't start more than one search
-  // at the same time.
+  // Get the two numbers in the text boxes. This is the search range.
   searchButton.disabled = true;
 
-  // Create the worker.
-  worker = new Worker("PrimeWorker.js");
-
-  // Hook up to the onMessage event, so you can receive messages
-  // from the worker.
-  worker.onmessage = receivedWorkerMessage;
-
-  // Get the number range, and send it to the web worker.
   var fromNumber = document.getElementById("from").value;
   var toNumber = document.getElementById("to").value;
 
+  // Ordinarily you would give the web worker JavaScript in a separate
+  // file, using a line of code like this.  
+  //worker = new Worker("PrimeWorker.js");
+  
+  // Because this example is hosted on CodePen,
+  // which doesn't let you use separate JS files,
+  // we need to use a workaround to grab the
+  // PrimeWorker.js code from the findPrimes() function
+  // on this page.  
+  var blob = new Blob([document.querySelector('#FindPrimes').textContent]); 
+  blobURL = window.URL.createObjectURL(blob);
+
+  worker = new Worker(blobURL);
+  worker.onmessage = receivedWorkerMessage;
+  worker.onerror = workerError;
+  
   worker.postMessage(
-    {
-      from: fromNumber,
-      to: toNumber
-    }
+   { from: fromNumber,
+     to: toNumber
+   }
   );
 
-  // Let the user know that things are on their way.
-  statusDisplay.innerHTML = "A web worker is on the job (" +
-    fromNumber + " to " + toNumber + ") ...";
-
-
-  function receivedWorkerMessage(event) {
-    // Get the prime number list.
-    var primes = event.data;
-
-    // Copy the list to the page.
-    // Take the results (an array of prime numbers), loop over it,
-    // and paste all the primes together into one long piece of text.
-    var primeList = "";
-    for (var i = 0; i < primes.length; i++) {
-      primeList += primes[i];
-      if (i != primes.length - 1) primeList += ", ";
-    }
-
-    // Show the prime number list on the page.
-    var displayList = document.getElementById("primeContainer");
-    displayList.textContent = primeList;
-
-    // Update the status text to tell the user what just happened.
-    var statusDisplay = document.getElementById("status");
-    if (primeList.length == 0) {
-      statusDisplay.textContent = "Search didn't find any results.";
-    }
-    else {
-      statusDisplay.textContent = "The results are here!";
-    }
-
-    // Allow more searches.
-    searchButton.disabled = false;
-  }
-
+  statusDisplay.innerHTML = "A web worker is on the job ("+
+   fromNumber + " to " + toNumber + ") ...";  
 }
 
+function receivedWorkerMessage(event) {
+  var message = event.data;
 
+  if (message.messageType == "PrimeList") {
+    var primes = message.data;
 
+    // Show the prime number list on the page.
+    var primeList = "";
+    for (var i=0; i<primes.length; i++) {
+      primeList += primes[i];
+      if (i != primes.length-1) primeList += ", ";
+    }
+    var displayList = document.getElementById("primeContainer");
+    displayList.innerHTML = primeList;
 
+    if (primeList.length == 0) {
+      statusDisplay.innerHTML = "Search failed to find any results.";
+    }
+    else {
+      statusDisplay.innerHTML = "The results are here!";
+    }
+    searchButton.disabled = false;
+  }
+  else if (message.messageType == "Progress") {
+    statusDisplay.innerHTML = message.data + "% done ...";
+  }
+}
 
+function workerError(error) {
+  statusDisplay.innerHTML = error.message;
+}
 
-
+function cancelSearch() {
+  worker.terminate();
+  worker = null;
+  statusDisplay.innerHTML = "Search cancelled.";
+  searchButton.disabled = false;
+}
